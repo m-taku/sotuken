f4c91e028a57a08c41e0778a5a9e864a3b58e744
somamizuki <somamizuki@gmail.com>
2019-09-17 18:05:53 +0900

    a

--- GameTemplate/Game/GameCamera.cpp
+++ GameTemplate/Game/GameCamera.cpp
@@ -1,12 +1,31 @@
 #include "stdafx.h"
 #include "GameCamera.h"
 
+namespace smEngine {
+	GameCamera::GameCamera()
+	{
+	}
 
-GameCamera::GameCamera()
-{
+	GameCamera::~GameCamera()
+	{
+	}
+
+	void GameCamera::Init()
+	{
+		m_position = CVector3::Zero();
+		m_target = CVector3::Back()*MAXDIST;
+		g_camera3D.SetPosition(m_position);
+		g_camera3D.SetTarget(m_target);
+		g_camera3D.SetUp(CVector3::Up());
+	}
+
+	void GameCamera::Update()
+	{
+
+
+		g_camera3D.SetPosition(m_position);
+		g_camera3D.SetTarget(m_target);
+		g_camera3D.SetUp(CVector3::Up());
+	}
 }
 
-
-GameCamera::~GameCamera()
-{
-}
