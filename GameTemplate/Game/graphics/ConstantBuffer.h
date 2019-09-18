d4f9934d4341c2e88a25a49db1ed0d57836beb14
somamizuki <somamizuki@gmail.com>
2019-09-18 13:58:35 +0900

    a

---
+++ GameTemplate/Game/graphics/ConstantBuffer.h
@@ -0,0 +1,30 @@
+#pragma once
+class ConstantBuffer
+{
+public:
+	ConstantBuffer();
+	~ConstantBuffer();
+	/*!
+	*@brief ConstantBufferを作成。
+	*@param[in]	pInitData		初期データ。
+	*@param[in]	bufferSize		バッファサイズ。
+	*/
+	bool Create(const void* pInitData, int bufferSize);
+	/*!
+	 * @brief	ID3D11Buffer*を取得。
+	 */
+	ID3D11Buffer*& GetBody()
+	{
+		return m_gpuBuffer;
+	}
+	/*!
+	*@brief	開放。
+	*@details
+	* この関数はデストラクタからもコールされています。
+	* 明示的なタイミングで開放したい場合に呼び出してください。
+	*/
+	void Release();
+private:
+	ID3D11Buffer* m_gpuBuffer = nullptr;
+};
+
