9798fb6b714f83909c44064f273905465c246768
m-taku <kbc17b19@stu.kawahara.ac.jp>
2019-11-12 15:09:11 +0900

    23132132113211231231231

--- GameTemplate/Game/Enemy.cpp
+++ GameTemplate/Game/Enemy.cpp
@@ -4,9 +4,33 @@
 
 Enemy::Enemy()
 {
+	m_characon.Init(
+		50.0f,
+		80.0f,
+		m_position
+	);
 }
 
 
 Enemy::~Enemy()
 {
+}
+bool Enemy::Start()
+{
+	m_skinmodel.Init(L"Assets/modelData/unityChan.cmo");
+	m_skinmodel.EnableShadowCaster(true);
+	return true;
+}
+void Enemy::Update()
+{
+	m_position = m_characon.Execute(GetFrameDeltaTime(), m_movespeed);
+	m_skinmodel.UpdateWorldMatrix(m_position, m_rotation, m_scale);
+}
+void Enemy::Draw()
+{
+	m_skinmodel.Draw(
+		enNormal,
+		smGameCamera().GetCameraViewMatrix(),
+		smGameCamera().GetCameraProjectionMatrix()
+	);
 }
