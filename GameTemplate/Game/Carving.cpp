ba19414731ad96302dcb03c1a1d18f6709217c18
sogabesatoshi <kbc17b14@stu.kawahara.ac.jp>
2019-11-15 15:32:06 +0900

    確率を引き算に変更

--- GameTemplate/Game/Carving.cpp
+++ GameTemplate/Game/Carving.cpp
@@ -22,19 +22,17 @@ void Carving::AddItem(std::string name, int value)
 
 std::string Carving::GetItem()
 {
-	int s, count = 0;
+	int s, count = 0, sub = 100;
 
 	s = rand() % 100;
 
 	for (auto itr = m_item.begin(); itr != m_item.end(); ++itr)
 	{
-		for (int i = 0; i < itr->second; i++)
+		sub -= itr->second;
+		if (s >= sub)
 		{
-			if (count == s)
-			{
-				return itr->first.c_str();
-			}
-			count++;
+			return itr->first.c_str();
+			break;
 		}
 	}
 	
